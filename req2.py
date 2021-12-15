e82249027fd03ac4875d019d967b92ddad3edc05
qasem-moh <qasemcoderjo2020@gmail.com>
2021-12-15 21:52:16 +0200

    app

--- req2.py
+++ req2.py
@@ -19,6 +19,8 @@ soup = BeautifulSoup(req, 'html.parser')
 
 # print(find.prettify())
 
+####################By ID##################
+
 byid = soup.find(id='pageName')
 print(byid)
 
@@ -29,4 +31,18 @@ all_by_id = soup.find_all(id='pageName')
 print(all_by_id)
 
 all_by_id2 = soup.find_all("h2", id='pageName')
+print(all_by_id2)
+
+####################By class##################
+
+byid = soup.find(class_='pageName')
+print(byid)
+
+byid2 = soup.find("h2", class_='pageName')
+print(byid2)
+
+all_by_id = soup.find_all(class_='pageName')
+print(all_by_id)
+
+all_by_id2 = soup.find_all("h2", class_='pageName')
 print(all_by_id2)
