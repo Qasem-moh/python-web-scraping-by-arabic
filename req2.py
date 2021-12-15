982ed9636971b40d695c72b2a182821f7d4e686f
qasem-moh <qasemcoderjo2020@gmail.com>
2021-12-15 21:47:27 +0200

    app

---
+++ req2.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://testphp.vulnweb.com/index.php"
+
+req = requests.get(url).text
+
+soup = BeautifulSoup(req, 'html.parser')
+
+# find = soup.find("h2")
+
+# find_all= soup.find_all("a")
+
+# for link in find_all:
+#     print(link)
+
+
+# print(find.text)
+
+# print(find.prettify())
+
+byid = soup.find(id='pageName')
+print(byid)
+
+byid2 = soup.find("h2", id='pageName')
+print(byid2)
+
+all_by_id = soup.find_all(id='pageName')
+print(all_by_id)
+
+all_by_id2 = soup.find_all("h2", id='pageName')
+print(all_by_id2)
